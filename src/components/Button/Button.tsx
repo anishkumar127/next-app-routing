c69960d6412203e9072f1feb580bdf8151387b9b
Anish Kumar <anishbishnoi127@gmail.com>
2023-06-07 22:16:43 +0530

    server action & client

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,28 @@
 "use client";
-import React from "react";
+import { SampleActions } from "@/actions/SampleActions";
+import React, { useState, useTransition } from "react";
 
 const Button = () => {
-  const handleClick = () => {
+  const [isPending, startTransition] = useTransition();
+  const [response, setResponse] = useState<any>({});
+  const handleClick = async () => {
     console.log("handle click triggering...");
+
+    // server actions function triggering.
+    startTransition(async () => {
+      const response = await SampleActions("Message for server");
+      console.log(response);
+      setResponse(response);
+    });
   };
-  return <button onClick={handleClick}>Button</button>;
+  return (
+    <div>
+      {isPending && <h1>Loading...</h1>}
+      <button onClick={handleClick}>Button</button>
+
+      <pre>{JSON.stringify(response, null, 4)}</pre>
+    </div>
+  );
 };
 
 export default Button;
